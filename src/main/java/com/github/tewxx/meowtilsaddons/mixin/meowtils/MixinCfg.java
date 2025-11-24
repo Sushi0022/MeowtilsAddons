62b9a2228e92bf560db9aaeb78d7b3e5eb5d1d19
Sushi <50385653+Sushi0022@users.noreply.github.com>
2025-11-24 15:42:21 -0500

    Cordinates for Quest Tracker HUD Editor

--- src/main/java/com/github/tewxx/meowtilsaddons/mixin/meowtils/MixinCfg.java
+++ src/main/java/com/github/tewxx/meowtilsaddons/mixin/meowtils/MixinCfg.java
@@ -43,6 +43,9 @@ public class MixinCfg {
 
     public boolean trackerModule = false;
     public int trackerModuleKey = 0;
+    public int questHUD_x = 5;
+    public int questHUD_y = 150;
+    public float questHUDScale = 1.0f;
 
     public boolean nameSpoofer = false;
     public int nameSpooferKey = 0;
